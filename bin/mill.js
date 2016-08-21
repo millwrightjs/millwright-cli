3c0ec8ff11b00d6508c235929cb6e3895a51b85d
Shawn Erquhart <shawn@professant.com>
2016-08-20 21:37:05 -0400

    initial commit

---
+++ bin/mill.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const argv = require('yargs').argv;
+
+try {
+  require(path.join(process.cwd(), 'node_modules/millwright'))(argv._[0]);
+}
+catch (e) {
+  console.log('Can\'t find Millwright.js - make sure you\'re in your project\'s root directory.');
+}
