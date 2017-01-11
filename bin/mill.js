b227f6fe9fea6b487042371252e3e0191f17a523
Shawn Erquhart <shawn@professant.com>
2017-01-11 09:51:55 -0500

    log all errors

--- bin/mill.js
+++ bin/mill.js
@@ -7,5 +7,6 @@ try {
   require(path.join(process.cwd(), 'node_modules/millwright'))(argv._[0]);
 }
 catch (e) {
+  console.log(e);
   console.log('Can\'t find Millwright.js - make sure you\'re in your project\'s root directory.');
 }
